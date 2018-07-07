b8e1192beca84086c6d44e81b9ac6ac33f7b8f27
T.M <38026296+tattooday@users.noreply.github.com>
2018-07-07 14:39:13 +0800

    Update Parabolic SAR.py

--- Parabolic SAR.py
+++ Parabolic SAR.py
@@ -1,11 +1,17 @@
 
 # coding: utf-8
 
-# In[131]:
-
-
-#https://en.wikipedia.org/wiki/Parabolic_SAR
-#https://www.box.com/s/gbtrjuoktgyag56j6lv0
+#parabolic stop and reverse is very useful for trend following
+#sar is an indicator below the price when its an uptrend 
+#and above the price when its a downtrend
+#it is very painful to calculate sar, though
+#and many explanations online including wiki cannot clearly explain the process
+#hence, the good idea would be to read info on wikipedia
+#and download an excel spreadsheet made by joeu2004
+#formulas are always more straight forward than descriptions
+#links are shown below
+# https://en.wikipedia.org/wiki/Parabolic_SAR
+# https://www.box.com/s/gbtrjuoktgyag56j6lv0
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,20 +19,26 @@ import fix_yahoo_finance as yf
 import pandas as pd
 
 
-# In[127]:
-
-
+#download data via fix yahoo finance library
 stdate=('2016-01-01')
 eddate=('2018-01-01')
 ticker=('EA')
+
+#slice is used for plotting
+#a two year dataset with 500 variables would be too much for a figure
 slicer=450
+
 df=yf.download(ticker,start=stdate,end=eddate)
 
 
-# In[149]:
+#the calculation of sar
+#as rules are very complicated
+#plz check the links above to understand more about it
 
 def parabolic_sar(new):
     
+    #this is common accelerating factors for forex and commodity
+    #for equity, af for each step could be set to 0.01
     initial_af=0.02
     step_af=0.02
     end_af=0.2
@@ -38,14 +50,14 @@ def parabolic_sar(new):
     new['ep']=0.0
     new['af']=0.0
 
-
+    #initial values for recursive calculation
     new['trend'][1]=1 if new['Close'][1]>new['Close'][0] else -1
     new['sar'][1]=new['High'][0] if new['trend'][1]>0 else new['Low'][0]
     new.set_value(1,'real sar',new['sar'][1])
     new['ep'][1]=new['High'][1] if new['trend'][1]>0 else new['Low'][1]
     new['af'][1]=initial_af
 
-
+    #calculation
     for i in range(2,len(new)):
         
         temp=new['sar'][i-1]+new['af'][i-1]*(new['ep'][i-1]-new['sar'][i-1])
@@ -80,7 +92,11 @@ def parabolic_sar(new):
     return new
 
 
-# In[158]:
+#generating signals
+#idea is the same as macd oscillator
+#check the website below to learn more
+# https://github.com/tattooday/quant-trading/blob/master/MACD%20oscillator%20backtest.py
+
 def signal_generation(df,method):
     
         new=method(df)
@@ -91,12 +107,9 @@ def signal_generation(df,method):
         
         return new
 
-
-
-
-
-
-# In[164]:
+    
+#plotting of sar and trading positions
+#still similar to macd
 
 def plot(new,ticker):
     
@@ -115,15 +128,20 @@ def plot(new,ticker):
     plt.show()
 
 
-#
+#delete adj close and volume
+#as we dont need them
 del df['Adj Close']
 del df['Volume']
 
+#no need to iterate over timestamp index
 df.reset_index(inplace=True)
 
 new=signal_generation(df,parabolic_sar)
 
+#convert back to time series for plotting
+#so that we get a date x axis
 new.set_index(new['Date'],inplace=True)
-new=new[slicer:]
 
+#shorten our plotting horizon and plot
+new=new[slicer:]
 plot(new,ticker)
