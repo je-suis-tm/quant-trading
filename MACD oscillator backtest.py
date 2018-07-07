81af350d2c51659ec0b0afb44fb82bee891de391
T.M <38026296+tattooday@users.noreply.github.com>
2018-07-07 10:10:24 +0100

    Update MACD oscillator backtest.py

--- MACD oscillator backtest.py
+++ MACD oscillator backtest.py
@@ -6,14 +6,12 @@ Created on Tue Feb  6 11:57:46 2018
 """
 
 #need to get fix yahoo finance package first
-import os
-os.chdir('D:/')
-os.getcwd()
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import fix_yahoo_finance as yf
-""""""
+
 #input the long moving average and short moving average period
 #for the classic MACD, it is 12 and 26
 #once a upon a time you got six trading days in a week
@@ -21,21 +19,28 @@ import fix_yahoo_finance as yf
 #for now, the ideal choice would be 10 and 21
 ma1=int(input('ma1:'))
 ma2=int(input('ma2:'))
-
 stdate=input('start date in format yyyy-mm-dd:')
 eddate=input('end date in format yyyy-mm-dd:')
 ticker=input('ticker:')
+
 #slicing the downloaded dataset
 #if the dataset is too large, backtesting plot would look messy
 #you get too many markers cluster together
-st=int(input('slicing:'))
+slicer=int(input('slicing:'))
 
-
-#downloading data and calculating moving average
+#downloading data
 df=yf.download(ticker,start=stdate,end=eddate)
-signals=pd.DataFrame(df)
-signals['ma1']=signals['Close'].rolling(window=ma1,min_periods=1,center=False).mean()
-signals['ma2']=signals['Close'].rolling(window=ma2,min_periods=1,center=False).mean()
+
+
+#simple moving average
+def macd(signals):
+    
+    
+    signals['ma1']=signals['Close'].rolling(window=ma1,min_periods=1,center=False).mean()
+    signals['ma2']=signals['Close'].rolling(window=ma2,min_periods=1,center=False).mean()
+    
+    return signals
+
 
 #signal generation
 #when the short moving average is larger than long moving average, we long and hold
@@ -44,50 +49,68 @@ signals['ma2']=signals['Close'].rolling(window=ma2,min_periods=1,center=False).m
 #we can subtract short moving average from long moving average
 #the difference between is sometimes positive, it sometimes becomes negative
 #thats why it is named as moving average converge/diverge oscillator
+def signal_generation(df,method):
+    
+    signals=method(df)
+    signals['positions']=0
 
-signals['signals']=0
-#signal becomes and stays one once the short moving average is above long moving average
-signals['signals'][ma1:]=np.where(signals['ma1'][ma1:]>=signals['ma2'][ma1:],1,0)
-#as signals only imply the holding
-#we take the difference to generate real trade signal
-signals['trade']=signals['signals'].diff()
-#oscillator is the difference between two moving average
-#when it is positive, we long, vice versa
-signals['oscillator']=signals['ma1']-signals['ma2']
-#take the slicing
-new=signals[st:]
+    #positions becomes and stays one once the short moving average is above long moving average
+    signals['positions'][ma1:]=np.where(signals['ma1'][ma1:]>=signals['ma2'][ma1:],1,0)
+
+    #as positions only imply the holding
+    #we take the difference to generate real trade signal
+    signals['signals']=signals['positions'].diff()
+
+    #oscillator is the difference between two moving average
+    #when it is positive, we long, vice versa
+    signals['oscillator']=signals['ma1']-signals['ma2']
+
+    return signals
+
 
 #plotting the backtesting result
-#the first plot is the actual close price with long/short positions
-fig=plt.figure()
-ax=fig.add_subplot(111)
-new['Close'].plot(label=ticker)
-ax.plot(new.loc[new['trade']==1].index,new['Close'][new['trade']==1],label='LONG',lw=0,marker='^',c='g')
-ax.plot(new.loc[new['trade']==-1].index,new['Close'][new['trade']==-1],label='SHORT',lw=0,marker='v',c='r')
-plt.legend(loc='best')
-plt.grid(True)
-plt.title('Positions')
-plt.show()
+def plot(new):
+    
+    #the first plot is the actual close price with long/short positions
+    fig=plt.figure()
+    ax=fig.add_subplot(111)
+    
+    new['Close'].plot(label=ticker)
+    ax.plot(new.loc[new['signals']==1].index,new['Close'][new['signals']==1],label='LONG',lw=0,marker='^',c='g')
+    ax.plot(new.loc[new['signals']==-1].index,new['Close'][new['signals']==-1],label='SHORT',lw=0,marker='v',c='r')
+
+    plt.legend(loc='best')
+    plt.grid(True)
+    plt.title('Positions')
+    
+    plt.show()
+    
+    #the second plot is long/short moving average with oscillator
+    #note that i use bar chart for oscillator
+    fig=plt.figure()
+    cx=fig.add_subplot(211)
+
+    new['oscillator'].plot(kind='bar',color='r')
+
+    plt.legend(loc='best')
+    plt.grid(True)
+    plt.xticks([])
+    plt.xlabel('')
+    plt.title('MACD Oscillator')
+
+    bx=fig.add_subplot(212)
+
+    new['ma1'].plot(label='ma1')
+    new['ma2'].plot(label='ma2',linestyle=':')
+    
+    plt.legend(loc='best')
+    plt.grid(True)
+    plt.show()
 
 
-#the second plot is long/short moving average with oscillator
-#note that i use bar chart for oscillator
-fig=plt.figure()
-cx=fig.add_subplot(211)
-new['oscillator'].plot(kind='bar',color='r')
-plt.legend(loc='best')
-plt.grid(True)
-plt.xticks([])
-plt.xlabel('')
-plt.title('MACD Oscillator')
-bx=fig.add_subplot(212)
-new['ma1'].plot(label='ma1')
-new['ma2'].plot(label='ma2',linestyle=':')
-plt.legend(loc='best')
-plt.grid(True)
-plt.show()
+new=signal_generation(df,macd)
+new=new[slicer:]
+plot(new)
 
-
-plt.show()
-#how to calculate stats could be found from my other code called Heikin-Ashi: https://github.com/tattooday/quant-trading/blob/master/heikin%20ashi%20backtest.py
-
+#how to calculate stats could be found from my other code called Heikin-Ashi: 
+# https://github.com/tattooday/quant-trading/blob/master/heikin%20ashi%20backtest.py
