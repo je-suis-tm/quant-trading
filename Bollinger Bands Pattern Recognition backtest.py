a9de8556253c26608fa43cf4aec69c12ab001383
T.M <38026296+tattooday@users.noreply.github.com>
2018-07-21 10:03:46 +0100

    Update Bollinger Bands Pattern Recognition backtest.py

--- Bollinger Bands Pattern Recognition backtest.py
+++ Bollinger Bands Pattern Recognition backtest.py
@@ -13,9 +13,10 @@
 #why machine learning when u can use arithmetic approach 
 #which is much faster and simpler?
 
-#there are many patterns
+#there are many patterns for recognition
 #top m, bottom w, head-shoulder top, head-shoulder bottom, elliott waves
 #in this content, we only discuss bottom w
+#top m is just the reverse of bottom w
 #rules of bollinger bands and bottom w can be found in the following link:
 # https://www.tradingview.com/wiki/Bollinger_Bands_(BB)
 
@@ -27,14 +28,18 @@ import numpy as np
 
 
 # In[2]:
-
-
 os.chdir('d:/')
+
+#again, i download data from histdata.com
+#and i take the average of bid and ask price
+df=pd.read_csv('gbpusd.csv')
 
 
 # In[3]:
 
-
+#first step is to calculate moving average and moving standard deviation
+#we plus/minus two standard deviations on moving average
+#we get our upper, mid, lower bands
 def bollinger_bands(df):
     
     data=copy.deepcopy(df)
@@ -49,14 +54,43 @@ def bollinger_bands(df):
 # In[4]:
 
 
+#the signal generation is a bit tricky
+#there are four conditions to satisfy
+#for the shape of w, there are five nodes
+#from left to right, top to bottom, l,k,j,m,i
+#when we generate signals
+#the iteration node is the top right node i, condition 4
+#first, we find the middle node j, condition 2
+#next, we identify the first bottom node k, condition 1
+#after that, we point out the first top node l
+#l is not any of those four conditions
+#we just use it for pattern visualization
+#finally, we locate the second bottom node m, condition 3
+#plz refer to the following link for my poor visualization
+# https://github.com/tattooday/quant-trading/blob/master/preview/bottom%20w%20pattern.png
 def signal_generation(data,method):
     
+    #according to investopedia
+    #for a double bottom pattern
+    #we should use 3-month horizon which is 75
     period=75
+    
+    #alpha denotes the difference between price and bollinger bands
+    #if alpha is too small, its unlikely to trigger a signal
+    #if alpha is too large, its too easy to trigger a signal
+    #which gives us a higher probability to lose money
+    #beta denotes the scale of bandwidth
+    #when bandwidth is larger than beta, it is expansion period
+    #when bandwidth is smaller than beta, it is contraction period
     alpha=0.0001
     beta=0.0001
     
     df=method(data)
     df['signals']=0
+    
+    #as usual, cumsum denotes the holding position
+    #coordinates store five nodes of w shape
+    #later we would use these coordinates to draw a w shape
     df['cumsum']=0
     df['coordinates']=''
     
@@ -150,7 +184,7 @@ def plot(new):
 # In[6]:
 
 
-df=pd.read_csv('gbpusd.csv')
+
 
 
 # In[7]:
