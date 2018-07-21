9aeed97969826855355905c1071d3ecd023ed7a7
T.M <38026296+tattooday@users.noreply.github.com>
2018-07-21 10:17:20 +0100

    Update Bollinger Bands Pattern Recognition backtest.py

--- Bollinger Bands Pattern Recognition backtest.py
+++ Bollinger Bands Pattern Recognition backtest.py
@@ -94,25 +94,34 @@ def signal_generation(data,method):
     df['cumsum']=0
     df['coordinates']=''
     
-    # https://github.com/tattooday/quant-trading/blob/master/preview/bottom%20w%20pattern.png
     for i in range(period,len(df)):
         
+        #moveon is a process control
+        #if moveon==true, we move on to verify the next condition
+        #if false, we move on to the next iteration
+        #threshold denotes the value of node k
+        #we would use it for the comparison with node m
+        #plz refer to condition 3
         moveon=False
         threshold=0.0
         
         #bottom w pattern recognition
         #condition 4
-        if (df['price'][i]>df['upper band'][i]) and         (df['cumsum'][i]==0):
+        if (df['price'][i]>df['upper band'][i]) and \
+        (df['cumsum'][i]==0):
             
             for j in range(i,i-period,-1):                
+                
                 #condition 2
-                if (np.abs(df['mid band'][j]-df['price'][j])<alpha) and                 (np.abs(df['mid band'][j]-df['upper band'][i])<alpha):
+                if (np.abs(df['mid band'][j]-df['price'][j])<alpha) and \
+                (np.abs(df['mid band'][j]-df['upper band'][i])<alpha):
                     moveon=True
                     break
             
             if moveon==True:
                 moveon=False
                 for k in range(j,i-period,-1):
+                    
                     #condition 1
                     if (np.abs(df['lower band'][k]-df['price'][k])<alpha):
                         threshold=df['price'][k]
@@ -122,6 +131,7 @@ def signal_generation(data,method):
             if moveon==True:
                 moveon=False
                 for l in range(k,i-period,-1):
+                    
                     #this one is for plotting w shape
                     if (df['mid band'][l]<df['price'][l]):
                         moveon=True
@@ -130,8 +140,11 @@ def signal_generation(data,method):
             if moveon==True:
                 moveon=False        
                 for m in range(i,j,-1):
+                    
                     #condition 3
-                    if (df['price'][m]-df['lower band'][m]<alpha) and                     (df['price'][m]>df['lower band'][m]) and                     (df['price'][m]<threshold):
+                    if (df['price'][m]-df['lower band'][m]<alpha) and \
+                    (df['price'][m]>df['lower band'][m]) and \
+                    (df['price'][m]<threshold):
                         df.set_value(i,'signals',1)
                         df.set_value(i,'coordinates','%s,%s,%s,%s,%s'%(l,k,j,m,i))
                         df['cumsum']=df['signals'].cumsum()
@@ -139,7 +152,17 @@ def signal_generation(data,method):
                         break
         
         #clear our positions when there is contraction on bollinger bands
-        if (df['cumsum'][i]!=0) and         (df['std'][i]<beta) and         (moveon==False):
+        #contraction on the bandwidth is easy to understand
+        #when price momentum exists, the price would move dramatically for either direction
+        #which greatly increases the standard deviation
+        #when the momentum vanishes, we clear our positions
+        
+        #note that we put moveon in the condition
+        #just in case our signal generation time is contraction period
+        #but we dont wanna clear positions right now
+        if (df['cumsum'][i]!=0) and \
+        (df['std'][i]<beta) and \
+        (moveon==False):
             df.set_value(i,'signals',-1)
             df['cumsum']=df['signals'].cumsum()
             
@@ -148,31 +171,42 @@ def signal_generation(data,method):
 
 # In[5]:
 
-
+#visualization
 def plot(new):
     
+    #as usual we could cut the dataframe into a small slice
+    #for a tight and neat figure
+    #a and b denotes entry and exit of a trade
     a,b=list(new[new['signals']!=0].iloc[:2].index)
     
     newbie=new[a-85:b+30]
     newbie.set_index(pd.to_datetime(newbie['date'],format='%Y-%m-%d %H:%M:%S'),inplace=True)
 
-    
+   
     fig=plt.figure(figsize=(10,5))
     ax=fig.add_subplot(111)
-
+    
+    #plotting positions on price series and bollinger bands
     ax.plot(newbie['price'],label='price')
     ax.fill_between(newbie.index,newbie['lower band'],newbie['upper band'],alpha=0.2,color='#45ADA8')
     ax.plot(newbie['mid band'],linestyle='--',label='moving average',c='#132226')
-    ax.plot(newbie['price'][newbie['signals']==1],marker='^',markersize=12,            lw=0,c='g',label='LONG')
-    ax.plot(newbie['price'][newbie['signals']==-1],marker='v',markersize=12,            lw=0,c='r',label='SHORT')
+    ax.plot(newbie['price'][newbie['signals']==1],marker='^',markersize=12, \
+            lw=0,c='g',label='LONG')
+    ax.plot(newbie['price'][newbie['signals']==-1],marker='v',markersize=12, \
+            lw=0,c='r',label='SHORT')
     
+    #plotting w shape
+    #we locate the coordinates then find the exact date as index
     temp=newbie['coordinates'][newbie['signals']==1]
     indexlist=list(map(int,temp[temp.index[0]].split(',')))
-    ax.plot(newbie['price'][pd.to_datetime(new['date'].iloc[indexlist])],            lw=5,alpha=0.7,c='#FE4365',label='double bottom pattern')
+    ax.plot(newbie['price'][pd.to_datetime(new['date'].iloc[indexlist])], \
+            lw=5,alpha=0.7,c='#FE4365',label='double bottom pattern')
     
-    
-    plt.text((newbie.loc[newbie['signals']==1].index[0]),             newbie['lower band'][newbie['signals']==1],'Expansion',fontsize=15,color='563838')
-    plt.text((newbie.loc[newbie['signals']==-1].index[0]),             newbie['lower band'][newbie['signals']==-1],'Contraction',fontsize=15,color='563838')
+    #add some captions
+    plt.text((newbie.loc[newbie['signals']==1].index[0]), \
+             newbie['lower band'][newbie['signals']==1],'Expansion',fontsize=15,color='563838')
+    plt.text((newbie.loc[newbie['signals']==-1].index[0]), \
+             newbie['lower band'][newbie['signals']==-1],'Contraction',fontsize=15,color='563838')
     
     plt.legend(loc='best')
     plt.title('Bollinger Bands Pattern Recognition')
@@ -183,19 +217,11 @@ def plot(new):
 
 # In[6]:
 
-
-
-
-
-# In[7]:
-
-
+#ta-da
 signals=signal_generation(df,bollinger_bands)
-
-
-# In[8]:
-
 
 new=copy.deepcopy(signals)
 plot(new)
 
+#how to calculate stats could be found from my other code called Heikin-Ashi
+# https://github.com/tattooday/quant-trading/blob/master/heikin%20ashi%20backtest.py
