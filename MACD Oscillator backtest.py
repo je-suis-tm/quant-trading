ba13886936ce2eb14dbdacd5ae5445ead24b39c5
T.M <38026296+je-suis-tm@users.noreply.github.com>
2019-03-13 14:24:15 +0000

    change link

--- MACD Oscillator backtest.py
+++ MACD Oscillator backtest.py
@@ -134,7 +134,7 @@ def main():
 
 
 #how to calculate stats could be found from my other code called Heikin-Ashi
-# https://github.com/tattooday/quant-trading/blob/master/heikin%20ashi%20backtest.py
+# https://github.com/je-suis-tm/quant-trading/blob/master/heikin%20ashi%20backtest.py
 
 
 if __name__ == '__main__':
