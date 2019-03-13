acf9feb4ddcd18f7bf821a307910d3bb8578aa21
T.M <38026296+je-suis-tm@users.noreply.github.com>
2019-03-13 14:13:54 +0000

    minor change

--- Bollinger Bands Pattern Recognition backtest.py
+++ Bollinger Bands Pattern Recognition backtest.py
@@ -63,7 +63,7 @@ def bollinger_bands(df):
 #we just use it for pattern visualization
 #finally, we locate the second bottom node m, condition 3
 #plz refer to the following link for my poor visualization
-# https://github.com/tattooday/quant-trading/blob/master/preview/bollinger%20bands%20bottom%20w%20pattern.png
+# https://github.com/je-suis-tm/quant-trading/blob/master/preview/bollinger%20bands%20bottom%20w%20pattern.png
 def signal_generation(data,method):
     
     #according to investopedia
@@ -230,7 +230,7 @@ def main():
     plot(new)
 
 #how to calculate stats could be found from my other code called Heikin-Ashi
-# https://github.com/tattooday/quant-trading/blob/master/heikin%20ashi%20backtest.py
+# https://github.com/je-suis-tm/quant-trading/blob/master/heikin%20ashi%20backtest.py
 
 
 if __name__ == '__main__':
