323df202938566e4f98acd8e0e77cc5254fb4c2e
T.M <38026296+je-suis-tm@users.noreply.github.com>
2019-08-29 17:49:50 +0100

    ajouter remarque

--- Options Straddle backtest.py
+++ Options Straddle backtest.py
@@ -29,9 +29,14 @@
 #preferably at the same option price as well
 #otherwise asymmetric option price means there is more one-sided risk than the other
 #you may wanna consider strangle or strap/strip in this case
+#short straddle is literally shorting call option and put option of the same strike price and the same strike date
+#preferably at the same option price as well
+#long straddle has unlimited profit for upside movement and limited loss
+#short straddle has unlimited loss for upside movement and limited profit
+#short straddle is commonly used in a sideway market
 #long straddle is commonly used in event driven strategy
 
-#for instance, Brexit on 30th of October 2019, its do or die
+#for instance, brexit on 30th of October 2019, its do or die, no ifs and buts
 #if bojo delivers a no-deal Brexit, uk sterling gonna sink
 #or he secures a new deal without backstop from macron and merkel
 #even though unlikely, uk sterling gonna spike
@@ -90,6 +95,8 @@ def find_strike_price(df):
 
 # In[3]:
 
+#this function is merely data cleansing
+#merging option price information with spot price
 
 def straddle(options,spot,contractsize,strikeprice):
         
@@ -110,6 +117,9 @@ def straddle(options,spot,contractsize,strikeprice):
 
     df.columns=temp
     
+    #we multiply contract size with spot price here
+    #it makes our life a lot easier later with visualization
+
     df['spot']=df['spot'].apply(lambda x:x*contractsize)
     
     return df
@@ -118,6 +128,8 @@ def straddle(options,spot,contractsize,strikeprice):
 
 # In[4]:
 
+#signal generation is actually very simple
+#just find the option pair at the closest price we can
 
 def signal_generation(df,threshold):
     
@@ -131,8 +143,12 @@ def signal_generation(df,threshold):
 
 # In[5]:
 
-
+#ploting the payoff diagram
 def plot(df,strikeprice,contractsize):
+    
+    #finding trading signal
+    #if no signal is found
+    #we declare no suitable entry point for options straddle
     
     ind=df[df['signals']!=0].index
 
@@ -140,16 +156,28 @@ def plot(df,strikeprice,contractsize):
         print('Strike Price at',strikeprice,'\nNo trades available.\n')
         return 
     
+    #calculate how much profit we can gain outta this
+    
     profit=np.abs(
         df['spot'].iloc[-1]-int(strikeprice)*contractsize
     )-df['call'][ind[0]]-df['put'][ind[0]]
 
     y=[]
     
+    #we use these two variables to plot how much we can profit at different spot price
+    
     begin=round(int(strikeprice)*contractsize-5*(df['call'][ind[0]]+df['put'][ind[0]]),0)
     end=round(int(strikeprice)*contractsize+5*(df['call'][ind[0]]+df['put'][ind[0]]),0)+1
     
     x=list(np.arange(int(begin),int(end)))
+    
+    #as u can see from the pic
+    # https://github.com/je-suis-tm/quant-trading/blob/master/preview/options%20straddle%20payoff%20diagram.png
+    #we only make money (green color) if the spot price is outside of a range
+    #group1 and group2 are variables that indicate which range our line plot gets red/green color
+    #they keep track of the indices that we switch from profit to loss or from loss to profit
+    #as indices are always positive, we initialize them to negative values
+
     group1,group2=-10,-10
     for j in x:
         temp=np.abs(j-int(strikeprice)*contractsize)-(df['call'][ind[0]]+df['put'][ind[0]])
@@ -164,17 +192,27 @@ def plot(df,strikeprice,contractsize):
     ax.spines['bottom'].set_position(('data',0))
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
-
+    
+    #pnl in different colors, red is loss, green is profit
+    
     plt.plot(x[:group1],y[:group1],c='#57bc90',lw=5)
     plt.plot(x[group2:],y[group2:],c='#57bc90',lw=5)
     plt.plot(x[group1:group2],y[group1:group2],c='#ec576b',lw=5)
+    
+    #ploting strike price
+    
     plt.plot([int(strikeprice)*contractsize,
               int(strikeprice)*contractsize],
               [0,-(df['call'][ind[0]]+df['put'][ind[0]])],
               linestyle=':',lw=3,c='#ec576b',alpha=0.5)
+    
+    #ploting spot price
+    
     plt.axvline(df['spot'].iloc[-1],lw=5,
                 linestyle='--',c='#e5e338',alpha=0.5)
-
+    
+    #adding annotations
+    
     plt.annotate('Strike Price',
                  xy=(int(strikeprice)*contractsize,
                      0),
@@ -214,8 +252,11 @@ def plot(df,strikeprice,contractsize):
                                  facecolor='#c5c1c0'),
                  va='center',ha='left'
                  )
-
+    
+    #limit x ticks to 3 for a tidy look
+    
     plt.locator_params(axis='x',nbins=3)
+    
     plt.title(f'Long Straddle Options Strategy\nP&L {round(profit,2)}')
     plt.ylabel('Profit & Loss')
     plt.xlabel('Price',labelpad=50)
@@ -252,6 +293,9 @@ def main():
     spot.index=pd.to_datetime(spot.index)
     
     target=find_strike_price(aug)
+    
+    #we iterate through all the available option pairs
+    #to find the optimal strike price to maximize our profit
     
     for strikeprice in target:
       
